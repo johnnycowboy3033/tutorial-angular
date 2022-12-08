65e3d0abd1875b2512fa09267a0a43e44aef8a59
Johnny Cowboy <johnnycowboy3033@gmail.com>
2022-12-07 23:26:54 -0500

    Code Cleanup

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable,of} from "rxjs";
-import { TASKS } from '../mock-task';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
 import { Task } from '../Task';
 
 @Injectable({
