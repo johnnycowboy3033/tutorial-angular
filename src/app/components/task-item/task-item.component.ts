f546a395ef00f94fad0c57e8a7246435812e31f6
Johnny Cowboy <johnnycowboy3033@gmail.com>
2022-12-08 17:02:46 -0500

    16. Deleting Tasks (1:03:59)

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
-import { Component,OnInit, Input } from '@angular/core';
+import { Component,OnInit, Input, Output, EventEmitter  } from '@angular/core';
 import {Task} from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-task-item',
@@ -10,12 +11,19 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent implements  OnInit{
 
   @Input()  task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {
   }
 
   ngOnInit():void {
+  }
+
+  onDelete(task: Task) {
+    console.log("Delete Task Button has been clicked. For task: " + JSON.stringify( task ) );
+
+    this.onDeleteTask.emit(task);
   }
 
 }
