a7990a49c8b5972bcb23172b53595055c818e9d4
Johnny Cowboy <johnnycowboy3033@gmail.com>
2022-12-21 14:09:22 -0500

    clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AboutComponent } from './components/about/about.component';
 import { FaqsComponent } from './components/faqs/faqs.component';
-import { CopywithinComponent } from './components/array/copywithin/copywithin.component';
-import { InitializeComponent } from './components/array/initialize/initialize.component';
-import { ShowStatesComponent } from './components/array/show-states/show-states.component';
 
 const appRoutes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'faqs', component: FaqsComponent },
-  { path: 'array/copywithin', component: CopywithinComponent },
+  { path: 'faqs', component: FaqsComponent }
 ];
 
 @NgModule({
@@ -31,9 +27,6 @@ const appRoutes: Routes = [
     WelcomeComponent,
     AboutComponent,
     FaqsComponent,
-    CopywithinComponent,
-    InitializeComponent,
-    ShowStatesComponent
   ],
     imports: [
         BrowserModule,
