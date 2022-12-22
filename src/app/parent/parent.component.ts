d2a625d1271b2d9ead1a76c515d9b990dad9bce7
Johnny Cowboy <johnnycowboy3033@gmail.com>
2022-12-22 12:24:47 -0500

    init commit

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,29 +1,17 @@
-import { Component,  ViewChild, AfterViewInit } from '@angular/core';
-import { ChildComponent } from "../child/child.component";
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   template: `
     <div id="parent">
       <h1>Parent</h1>
-      Message : {{ message }}
       <app-child></app-child>
     </div>
   `,
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements AfterViewInit{
-
-  @ViewChild(ChildComponent) child: any;
-
-  message :string = '';
+export class ParentComponent{
 
   constructor() {
   }
-
-  ngAfterViewInit(): void {
-    this.message = this.child.message;
-  }
-
-
 }
